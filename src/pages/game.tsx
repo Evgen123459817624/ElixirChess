69fc416ce438a588bc76f606cb0a713e000aafce
Eugen Popusoi <popusoi.eug27@gmail.com>
2026-01-31 18:21:54 +0200

    Added the chess board

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -1,3 +1,41 @@
+import "./game.css";
+
 export function Game() {
-  return <div>Game Page</div>;
+  const board = [];
+
+  for (let i = 0; i < 64; i++) {
+    const row = Math.floor(i / 8);
+    const col = i % 8;
+    const isDark = (row + col) % 2 === 1;
+    let canSpawn: string = "";
+    if (row === 1 || row === 6) {
+      canSpawn = "pawn";
+    } else if (row === 0 || row === 7) {
+      if (col === 0 || col === 7) {
+        canSpawn = "rook";
+      } else if (col === 1 || col === 6) {
+        canSpawn = "knight";
+      } else if (col === 2 || col === 5) {
+        canSpawn = "bishop";
+      } else if (col === 3) {
+        canSpawn = "queen";
+      } else if (col === 4) {
+        canSpawn = "king";
+      }
+    }
+    board.push({ id: i, isDark, row, col, canSpawn });
+  }
+
+  return (
+    <>
+      <div className="board">
+        {board.map((cell) => (
+          <div
+            key={cell.id}
+            className={`cell ${cell.isDark ? "black" : "white"}`}
+          ></div>
+        ))}
+      </div>
+    </>
+  );
 }

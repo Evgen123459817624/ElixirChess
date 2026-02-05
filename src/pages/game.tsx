41a7cb1493318687338c989fab23625437e332b9
Eugen Popusoi <popusoi.eug27@gmail.com>
2026-02-05 14:06:43 +0200

    Implemented the blacks shop

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -2,6 +2,19 @@ import "./game.css";
 import { useState } from "react";
 import kingWhite from "../assets/king-white.png";
 import kingBlack from "../assets/king-black.png";
+import pawnBlack from "../assets/pawn-black.png";
+import knightBlack from "../assets/knight-black.png";
+import bishopBlack from "../assets/bishop-black.png";
+import rookBlack from "../assets/king-black.png";
+import queenBlack from "../assets/queen-black.png";
+
+const piecePrice: Record<string, number> = {
+  pawn: 1,
+  knight: 3,
+  bishop: 3,
+  rook: 5,
+  queen: 9,
+};
 
 interface Cell {
   id: number;
@@ -67,22 +80,73 @@ export function Game() {
   const [selectedPieceID, setSelectedPieceID] = useState<number | null>(null);
   const [colorTurn, setColorTurn] = useState<"white" | "black">("white");
 
+  const [elixirBlack, setElixirBlack] = useState<number>(0);
+  const [elixirWhite, setElixirWhite] = useState<number>(0);
+
+  const [selectedShopPiece, setSelectedShopPiece] = useState<
+    "pawn" | "knight" | "bishop" | "rook" | "queen" | null
+  >(null);
+
   const handleCellClick = (id: number) => {
+    //deselect logic
+    if (id === selectedPieceID) {
+      setSelectedPieceID(null);
+      return;
+    }
+
+    //first click - select a piece
     if (
       selectedPieceID === null &&
       board[id].currPiece !== null &&
       board[id].color === colorTurn
     ) {
       setSelectedPieceID(id);
+      setSelectedShopPiece(null);
       return;
     }
 
+    //second click - move the piece
     if (selectedPieceID !== null && id !== selectedPieceID) {
       movePiece(selectedPieceID, id);
       setSelectedPieceID(null);
+      setSelectedShopPiece(null);
       setColorTurn((prevColorTurn) =>
         prevColorTurn === "white" ? "black" : "white",
       );
+      if (colorTurn === "black")
+        setElixirBlack((prevElixirCost) => prevElixirCost + 1);
+    }
+
+    //placing a selected shop piece
+    if (selectedShopPiece !== null) {
+      if (colorTurn !== "black") return;
+
+      let cost = piecePrice[selectedShopPiece];
+
+      if (elixirBlack < cost || board[id].canSpawn !== selectedShopPiece)
+        return;
+
+      setBoard((prevBoard) => {
+        const newBoard = [...prevBoard];
+        newBoard[id] = {
+          ...newBoard[id],
+          color: "black",
+          currPiece: selectedShopPiece,
+          currImg:
+            selectedShopPiece === "pawn"
+              ? pawnBlack
+              : selectedShopPiece === "knight"
+                ? knightBlack
+                : selectedShopPiece === "bishop"
+                  ? bishopBlack
+                  : selectedShopPiece === "rook"
+                    ? rookBlack
+                    : queenBlack,
+        };
+        return newBoard;
+      });
+      setElixirBlack((prevElixir) => prevElixir - cost);
+      setSelectedShopPiece(null);
     }
   };
 
@@ -108,8 +172,45 @@ export function Game() {
     });
   };
 
+  const selectPieceShop = (
+    piece: "pawn" | "knight" | "bishop" | "rook" | "queen",
+  ) => {
+    if (colorTurn !== "black") return;
+    setSelectedPieceID(null);
+    setSelectedShopPiece((prevSelectedShopPiece) => {
+      return prevSelectedShopPiece === piece ? null : piece;
+    });
+  };
+
   return (
     <>
+      <div className="blackShop">
+        {elixirBlack}
+        <img
+          src={pawnBlack}
+          alt=""
+          onClick={() => selectPieceShop("pawn")}
+          className={selectedShopPiece === "pawn" ? "selectedShopPiece" : ""}
+        />
+        <img
+          src={knightBlack}
+          alt=""
+          onClick={() => selectPieceShop("knight")}
+          className={selectedShopPiece === "knight" ? "selectedShopPiece" : ""}
+        />
+        <img
+          src={bishopBlack}
+          alt=""
+          onClick={() => selectPieceShop("bishop")}
+          className={selectedShopPiece === "bishop" ? "selectedShopPiece" : ""}
+        />
+        <img
+          src={queenBlack}
+          alt=""
+          onClick={() => selectPieceShop("queen")}
+          className={selectedShopPiece === "queen" ? "selectedShopPiece" : ""}
+        />
+      </div>
       <div className="board">
         {board.map((cell) => (
           <div
